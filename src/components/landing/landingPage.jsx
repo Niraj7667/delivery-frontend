5d788b5bc4b6eb01af1601365c6b52cb16ffce5e
Niraj Kumar <nk931685@gmail.com>
2025-01-27 01:08:35 +0530

    adding css and making responsive

--- src/components/landing/landingPage.jsx
+++ src/components/landing/landingPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import "./LandingPage.css"
+import "./landingPage.css"
 const LandingPage = () => {
   const navigate = useNavigate();
 
